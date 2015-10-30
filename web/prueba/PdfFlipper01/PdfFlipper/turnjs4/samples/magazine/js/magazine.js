34268bbd77b791337717d51df4b8435823fe2355
ignachiou <salvador.ignacio.salvatierra@gmail.com>
2015-10-30 01:45:48 -0430

    contenedores visualizador
    Se diseña los contenedores laterales para seleccionar las imagenes, y
    otro contenedor donde se encontrara la información del libro

--- web/prueba/PdfFlipper01/PdfFlipper/turnjs4/samples/magazine/js/magazine.js
+++ web/prueba/PdfFlipper01/PdfFlipper/turnjs4/samples/magazine/js/magazine.js
@@ -48,7 +48,7 @@ function loadPage(page, pageElement) {
 
 	// Load the page
 
-	img.attr('src', 'pages/' +  page + '.jpg');
+	img.attr('src', <?php echo $path;?>' +  page + '.jpg');
 
 	loadRegions(page, pageElement);
 
@@ -179,7 +179,7 @@ function loadLargePage(page, pageElement) {
 
 	// Loadnew page
 	
-	img.attr('src', 'pages/' +  page + '-large.jpg');
+	img.attr('src', '<?php echo $path;?>' +  page + '-large.jpg');
 }
 
 // Load small page
@@ -193,7 +193,7 @@ function loadSmallPage(page, pageElement) {
 	img.unbind('load');
 	// Loadnew page
 
-	img.attr('src', 'pages/' +  page + '.jpg');
+	img.attr('src', '<?php echo $path;?>' +  page + '.jpg');
 }
 
 // http://code.google.com/p/chromium/issues/detail?id=128488
