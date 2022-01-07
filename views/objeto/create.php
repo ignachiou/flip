0eaaef1b05eeac2b3df1cb807a4241886e4e511d
Salvador Salvatierra <salvador.ignacio.salvatierra@gmail.com>
2022-01-06 22:16:34 -0600

    Naming Change
    Changing the title name.

--- views/objeto/create.php
+++ views/objeto/create.php
@@ -6,7 +6,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Objeto */
 
-$this->title = 'Almacenar Monográfia';
+$this->title = 'Guardar Monográfia';
 ?>
 <div class="objeto-create">
 
